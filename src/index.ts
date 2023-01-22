9ee90ed0e73b03fc06c4da3934f989db07ba444c
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-01-22 07:48:04 +0000

    Actually register cronJob

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,9 @@ import "./lib/express/express.js";
 import "./lib/discord/discord.js";
 import { checkStatus } from "./lib/checkStatus.js";
 import { QuickDB } from "quick.db";
+import cron from "node-cron";
 export const db = new QuickDB();
 checkStatus();
+cron.schedule('30 * * * *', () => {
+    checkStatus();
+}); 
